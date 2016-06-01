0076858f35fc6b7ee1caf11f3c8a6272a2de2f6d
Daniel Freedman <dfreedm@google.com>
2016-06-01 12:04:59 -0700

    Clean up test runner
    Drop broken-by-design tests from shady build

--- lib/test.sh
+++ lib/test.sh
@@ -1,15 +1,38 @@
 #!/usr/bin/env bash
+set -e
 cleanup() {
   pushd lib/polymer >/dev/null
   git checkout .
-  (cd ~/polymer/components/polymer; git diff) | patch -Np1
   popd >/dev/null
 }
-shadow() {
-  wct -l chrome test/runner.html
+
+prep_shadow() {
+  pushd lib/polymer >/dev/null
+  ../../bin/polymer-css-build test/unit/styling-scoped-elements.html test/unit/styling-scoped.html
+  ../../bin/polymer-css-build test/unit/styling-cross-scope-unknown-host.html
+  ../../bin/polymer-css-build test/unit/styling-cross-scope-var.html
+  ../../bin/polymer-css-build test/unit/styling-cross-scope-apply.html
+  ../../bin/polymer-css-build test/unit/custom-style.html test/unit/custom-style-import.html test/unit/sub/style-import.html
+  ../../bin/polymer-css-build test/unit/custom-style-late.html test/unit/custom-style-late-import.html
+  popd >/dev/null
 }
-shady() {
-  wct -l chrome test/shady-runner.html
+
+test_shadow() {
+  wct -l chrome -l firefox test/runner.html
+}
+
+prep_shady() {
+  pushd lib/polymer >/dev/null
+  ../../bin/polymer-css-build --build-for-shady test/unit/styling-scoped-elements.html test/unit/styling-scoped.html
+  ../../bin/polymer-css-build --build-for-shady test/unit/styling-cross-scope-var.html
+  ../../bin/polymer-css-build --build-for-shady test/unit/styling-cross-scope-apply.html
+  ../../bin/polymer-css-build --build-for-shady test/unit/custom-style.html test/unit/custom-style-import.html test/unit/sub/style-import.html
+  ../../bin/polymer-css-build --build-for-shady test/unit/custom-style-late.html test/unit/custom-style-late-import.html
+  popd >/dev/null
+}
+
+test_shady() {
+  wct -l chrome -l firefox test/shady-runner.html
 }
 
 # all normal
@@ -17,29 +40,13 @@ cleanup
 
 # shadow build
 echo "= shadow build ="
-pushd lib/polymer >/dev/null
-../../bin/polymer-css-build test/unit/styling-scoped-elements.html test/unit/styling-scoped.html
-../../bin/polymer-css-build test/unit/styling-cross-scope-unknown-host.html
-../../bin/polymer-css-build test/unit/styling-cross-scope-var.html
-../../bin/polymer-css-build test/unit/styling-cross-scope-apply.html
-../../bin/polymer-css-build test/unit/custom-style.html test/unit/custom-style-import.html test/unit/sub/style-import.html
-../../bin/polymer-css-build test/unit/custom-style-late.html test/unit/custom-style-late-import.html
+prep_shadow
+test_shadow
 
-popd >/dev/null
-shadow
-
+# revert
 cleanup
 
 # shady build
 echo "= shady build ="
-pushd lib/polymer >/dev/null
-../../bin/polymer-css-build --build-for-shady test/unit/styling-scoped-elements.html test/unit/styling-scoped.html
-../../bin/polymer-css-build --build-for-shady test/unit/styling-cross-scope-unknown-host.html
-../../bin/polymer-css-build --build-for-shady test/unit/styling-cross-scope-var.html
-../../bin/polymer-css-build --build-for-shady test/unit/styling-cross-scope-apply.html
-../../bin/polymer-css-build --build-for-shady test/unit/custom-style.html test/unit/custom-style-import.html test/unit/sub/style-import.html
-../../bin/polymer-css-build --build-for-shady test/unit/custom-style-late.html test/unit/custom-style-late-import.html
-popd >/dev/null
-shady
-
-cleanup
+prep_shady
+test_shady
